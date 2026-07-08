488949d9c4de9727ad5285c56c110c70c7e932d7
Musawer Afzal <musawerafzal23@gmail.com>
2026-07-08 23:25:18 +0500

    Kept for Workspaces

--- backend/app/store.py
+++ backend/app/store.py
@@ -1,17 +1,8 @@
 import uuid
 from datetime import datetime, timezone
 from fastapi import HTTPException
-from security import hash_password
 
 _WORKSPACES: dict[str, dict] = {}
-
-_USERS = {
-    "u1": {
-        "id": "u1",
-        "email": "admin@test.com",
-        "password_hash": hash_password("password123")
-    }
-}
 
 def _now():
     return datetime.now(timezone.utc)
@@ -26,15 +17,3 @@ def get_or_404(workspace_id: str) -> dict:
 
 def generate_id():
     return uuid.uuid4().hex # Now we can just call generate_id for uuid. Following DRY principle
-
-
-def get_user_by_email(email: str):
-    for user in _USERS.values():
-        if user["email"] == email:
-            return user
-        
-    return None
-
-
-def get_user_by_id(user_id: str):
-    return _USERS.get(user_id)
